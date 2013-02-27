b91228c62d84dd91969c86d90c3874d9e02d5752
Shajin <shajinvk@gmail.com>
2013-02-27 14:15:29 +0530

    Initial Commit - completed spree installation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CaveSpree::Application.config.secret_token = '2163fa5928b31a9cdec24e782798681bb99624e990c4436ef7a2fb95e13df47c9ed4dc1f2169af59f8d2ff8cfaae7bd3068f68c26c3415c3418b56dfff0f604f'
